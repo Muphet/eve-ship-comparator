efc5538a57308bc72f7aa0270e90c54ee0904dd1
Nick Husher <nick.husher@dealer.com>
2012-12-20 12:50:34 -0500

    Datatable. Sigh.

--- assets/js/ship-comparator.js
+++ assets/js/ship-comparator.js
@@ -4,16 +4,10 @@ YUI({
     insertBefore: 'main-styles'
 }).use('io', 'template', 'autocomplete', 'ship-model', 'datatable', 'datatable-sort', function(Y) {
 
+    /*
     var micro = new Y.Template();
 
-    var shipResult = micro.compile([
-            '<img class="ship-result-image" src="/img/ships/<%= this.id %>.png" width="32" height="32" />',
-            '<span class="ship-result-name"><%= this.name %></span>',
-            '<span class="ship-result-type"><%= this.type %></span>',
-            '<span class="ship-faction-name"><%= this.race %></span>'
-        ].join(''));
-
-        item = Y.one('#ship-picker').plug(Y.Plugin.AutoComplete, {
+    var item = Y.one('#ship-picker').plug(Y.Plugin.AutoComplete, {
             source: '/ship/byNameOrType/{query}',
             resultHighlighter: 'phraseMatch',
             resultTextLocator: 'name',
@@ -25,6 +19,57 @@ YUI({
                 });
             }
         });
+    */
+    
+    function sortNested(propertyPath) {
+        var getVal = Y.Object.getValue,
+            path = propertyPath.split('.'),
+            attr = path.shift();
+        
+        return function(a, b, desc) {
+            var va = a.get(attr),
+                vb = b.get(attr);
+        
+            return (getVal(va, path) - getVal(vb, path)) * (desc ? -1 : 1);
+        }
+    }
+
+    var table = new Y.DataTable({
+        columns: [
+            {
+                key: "id",
+                label: ' ',
+                allowHTML: true,
+                formatter: function(o) { return '<img src="/img/ships/' + o.value + '.png" />'; },
+                sortFn: function(a, b, desc) {
+                    var ra = a.get('race'),
+                        rb = b.get('race'),
+                        l = desc ? -1 : 1,
+                        r = l * -1;
+                    
+                    return ra === rb ? 0 : (ra > rb ? l : r);
+                }
+            },
+            {
+                key: "name",
+                label: "Ship Name"
+            },
+            {
+                key: "shield",
+                label: "Shield Recharge",
+                formatter: function(o) { return Math.round(o.value.peakRecharge * 100) / 100; },
+                sortFn: sortNested('shield.peakRecharge')
+            },
+            {
+                key: "capacitor",
+                label: "Cap Recharge",
+                formatter: function(o) { return Math.round(o.value.peakRecharge * 100) / 100; },
+                sortFn: sortNested('capacitor.peakRecharge')
+            }
+        ],
+        sortable: true
+    }).render('#ship-display');
+
 
 
     Y.io('/ship/byNameOrType/frigate', {
@@ -35,14 +80,11 @@ YUI({
                 
                 res.forEach(function(s) {
                     rows.push(new Y.esc.Ship(s));
-                })
+                });
 
-                console.log(rows);
+                table.addRows(rows);
+                
             }
         }
-    });
-
-    item.ac.on('select', function(evt) {
-
     });
 });
