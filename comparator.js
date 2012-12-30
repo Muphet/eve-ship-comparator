4b465e76112e5b004bcb949c35151475f0131442
Nick Husher <nhusher@gmail.com>
2012-12-30 04:01:50 -0500

    Rebuilt the template layer so I can share tempaltes backend and frontend

--- comparator.js
+++ comparator.js
@@ -1,11 +1,17 @@
 var express = require('express'),
     ship = require('./controllers/ship'),
+    util = require('./controllers/util'),
     app = express();
     
 app.use(express.static(__dirname + '/assets'));
 app.use(express.static(__dirname + '/model'));
 
-app.engine('html', require('ejs').renderFile);
+app.use(function(err, req, res, next) {
+    // console.error(err);
+    res.send(500, "Something bad happen!");
+});
+
+app.engine('html', require('./lib/micro-template'));
 
 app.set('view engine', 'html');
 
@@ -14,5 +20,7 @@ app.set('views', __dirname + '/views');
 app.get('/', ship.index);
 
 app.get('/compare?', ship.compare);
+
+app.get('/templates.js', util.tmpl);
 
 app.listen(process.env.PORT || 8080);
---
+++ controllers/util.js
@@ -0,0 +1,70 @@
+
+var fs = require('fs'),
+    Q  = require('q'),
+    MicroTemplate = require('../lib/micro-template');
+
+
+var TMPL_CACHE; // TODO: figure out somewhere better / some way better to do this
+
+var TMPL_PATH = '/views/shared/';
+
+function getTemplates() {
+    var d = Q.defer();
+    
+    if(TMPL_CACHE) {
+        d.resolve(TMPL_CACHE);
+    } else {
+        TMPL_CACHE = {};
+        fs.readdir('.' + TMPL_PATH, function(err, files) {
+            if(err) {
+                throw err;
+            } else {            
+                Q.all(files.map(function(f) { return getTemplate(f) })).then(function(tmpls) {
+                    TMPL_CACHE = tmpls;
+
+                    d.resolve(TMPL_CACHE);
+                });
+            }
+        });
+    }
+    
+    return d.promise;
+}
+
+function getTemplate(f) {
+    var d = Q.defer();
+    
+    fs.readFile('.' + TMPL_PATH + f, 'utf8', function(err, file) {
+        if(!err) {
+            d.resolve({
+                path: TMPL_PATH + f.split('.')[0],
+                template: MicroTemplate.precompile(file)
+            });
+        } else {
+            throw err;
+        }
+    });
+    
+    return d.promise;
+}
+
+exports.tmpl = function(req, res, next) {
+    getTemplates().then(function(tmpls) {
+        res.set('Content-Type', 'text/javascript');
+        
+        var out = [
+            'window.esc = window.esc || {};',
+            'window.esc.tmpl = {};'
+        ];
+        
+        
+        tmpls.forEach(function(t) {
+            out.push('window.esc.tmpl["' + t.path + '"] = ' + t.template);
+        });
+        
+        res.send(out.join('\n'));
+        
+    }).fail(function() {
+        console.log(arguments);
+    });
+};
