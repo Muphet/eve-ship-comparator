62582389d7f74216d2e598ea69096c11627a221f
Nick Husher <nhusher@gmail.com>
2013-02-20 15:52:48 -0500

    Fixed broken config json and added some better number formatting

--- shared/utils/template-helpers.js
+++ shared/utils/template-helpers.js
@@ -8,9 +8,22 @@ YUI.add('esc-template-helpers', function(Y) {
     NS.shipStatItem = MT.compile([
         '<${this.wrapper || "li"} class="${this.className} stat-item">',
         '<span class="label" title="${this.label}">${this.label}</span>',
-        '<span class="value">${this.value}</span>',
+        '<span class="value">${ $h.shipStat(this.value, this.percent) }</span>',
         '</${this.wrapper || "li"}>'
     ].join(''));
+
+    NS.shipStat = function(s, percent) {
+
+        if(percent) {
+            s = (s * 100).toFixed(0) + '%';
+        } else if(s > 1000) {
+            s = (Math.round(s/100)/10).toFixed(1) + 'k';
+        } else {
+            s = Math.round(s);
+        }
+
+        return s;
+    };
 
 }, '', {
     requires: [ 'esc-micro-template' ]
